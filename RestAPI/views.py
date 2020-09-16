749e5a6516436759344ff7926a68a95005607236
Ammaros <ammarstudent2000@gmail.com>
2020-09-16 11:56:29 +0500

    Returning Clean Data

--- RestAPI/views.py
+++ RestAPI/views.py
@@ -1,3 +1,5 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import SignUpSerializer
 from .models import SignUpModel
@@ -6,15 +8,15 @@ class SignUpViewSet(viewsets.ModelViewSet):
     queryset = SignUpModel.objects.all().order_by('id')
     serializer_class = SignUpSerializer
 
-    # @api_view(['POST'])
-    # def create_auth(request):
-    #     serialized = SignUpSerializer(data=request.DATA)
-    #     if serialized.is_valid():
-    #         User.objects.create_user(
-    #             serialized.init_data['email'],
-    #             serialized.init_data['name'],
-    #             serialized.init_data['password']
-    #         )
-    #         return Response(serialized.data, status=status.HTTP_201_CREATED)
-    #     else:
-    #         return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+    @api_view(['POST'])
+    def create_auth(request):
+        serialized = SignUpSerializer(data=request.DATA)
+        if serialized.is_valid():
+            User.objects.create_user(
+                serialized.init_data['email'],
+                serialized.init_data['name'],
+                serialized.init_data['password']
+            )
+            return Response(serialized.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
